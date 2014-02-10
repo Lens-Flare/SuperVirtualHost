52ca6506f6d62720f500fa8bd7c2de3c1282400a
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-02-09 19:30:36 -0500

    Adding starbound

---
+++ extensions/starbound/src/main/java/com/lensflare/svh/starbound/Host.java
@@ -0,0 +1,102 @@
+package com.lensflare.svh.starbound;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.lensflare.svh.Connection;
+import com.lensflare.svh.Server;
+import com.lensflare.svh.Service;
+
+public class Host extends com.lensflare.svh.impl.Host {
+	private static final Logger log = LogManager.getLogger();
+	
+	/**
+	 * @see com.lensflare.svh.impl.Host#Host(Server, String, Map)
+	 */
+	public Host(Server server, String name, Map<?, ?> config) throws Exception {
+		super(server, name, config);
+	}
+	
+	@Override
+	public void run() {
+		log.info("Running the {} host", getName());
+		
+		while (isRunning())
+			try {
+				log.debug("Accepting a connection");
+				Connection connection = accept();
+				
+				log.debug("Capturing the first packet");
+				byte[] data = connection.getData();
+				
+				log.debug("Determining client type");
+				Service service = null;
+				if (data[0] == (byte)0xFE)
+				{
+					log.debug("Pre-1.7 ping packet received");
+					service = getServer().getServiceForTypeAndHost("Minecraft", pre17ping);
+				}
+				// is it a pre-1.7 server connection
+				else if (data[0] == 2)
+				{
+					log.debug("Pre-1.7 connection packet received");
+					// user name
+					int offset = 2; // string length offset
+					int strlen = data[offset++] << 8 | data[offset++]; // string length
+					
+					// host name
+					offset += 2 * strlen; // string length offset
+					strlen = data[offset++] << 8 | data[offset++]; // string length
+					
+					// getting the service
+					service = getServer().getServiceForTypeAndHost("Minecraft", new String(data, offset, strlen * 2, "UTF-16"));
+				}
+				// is it a 1.7+ connection (ping or otherwise)?
+				else if (data[1] == 0)
+				{
+					log.debug("Post-1.7 packet received");
+					// host name
+					int offset = 3; // string length offset
+					int strlen = data[offset++]; // string length
+					
+					// getting the service
+					service = getServer().getServiceForTypeAndHost("Minecraft", new String(data, offset, strlen, "UTF-8"));
+				}
+				// no idea what it might be, print it out so someone can submit an issue
+				else {
+					log.error("Unknown packet received:");
+					
+					// print out the packet as characters (non-printing chars become '.')
+					System.out.print('\t');
+					for (int i = 0; i < data.length; i++)
+						if (20 <= data[i] && data[i] < 127)
+							System.out.print(String.format("%c", data[i]));
+						else
+							System.out.print('.');
+					System.out.println();
+					
+					// print out the packet as hex
+					System.out.print('\t');
+					for (int i = 0; i < data.length; i++) {
+						System.out.print(String.format("%02x", data[i]));
+						if (i % 4 == 3)
+							System.out.print(' ');
+					}
+					System.out.println();
+				}
+				
+				if (service == null) {
+					log.warn("No service found");
+					continue;
+				}
+				
+				log.debug("Passing off connection");
+				service.handleConnection(connection);
+			} catch (Exception e) {
+				log.catching(Level.WARN, e);
+			}
+	}
+}
